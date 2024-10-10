9099415f2a8abfe7333df1a807aea797097fa105
Tomás Gil <tommygilbravo@gmail.com>
2024-10-10 09:31:12 -0400

    Vista de Login y registro

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,5 +1,48 @@
+import { Link, useNavigate } from "react-router-dom";
+import styles from './LoginPage.module.css'
+import { homeUrl, registerUrl } from "../../constants/urls";
+
+
 export function LoginPage(){
-    return <div>
-        <h1>Login</h1>
+    return (
+    <div className={styles.container}>
+        <form className={styles.form}>
+            <h1 className={styles.title}>Inicia Sesión</h1>            
+            <div className={styles.inputContainer}>
+                <label htmlFor="nombre">
+                    <span>Nombre de Usuario o Correo</span>
+                </label>
+                <input
+                    type="text"
+                    name="nombre"
+                    id="nombre"
+                    placeholder="Jane Doe, o ejemplo@gmail.com"
+                />              
+            </div>
+
+            <div className={styles.inputContainer}>
+                <label htmlFor="password">
+                    <span>Contraseña</span>
+                </label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="********"
+                />              
+            </div>
+
+            <button className={styles.submit}
+                type="submit"
+            >
+                Inicia Sesión
+            </button>
+            
+            <Link to={registerUrl} className={styles.link}>¿No tienes cuenta? Regístrate aquí</Link>
+            <Link to={homeUrl} className={styles.link}>Regresar a la página principal</Link>
+
+
+        </form>
     </div>
+    )
 }
--- src/pages/Register/RegisterPage.jsx
+++ src/pages/Register/RegisterPage.jsx
@@ -1,5 +1,61 @@
+import { Link, useNavigate } from "react-router-dom";
+import styles from './RegisterPage.module.css'
+import { homeUrl, loginUrl, registerUrl } from "../../constants/urls";
+
 export function RegisterPage(){
-    return <div>
-        <h1>Registrar</h1>
+    const navigate = useNavigate();
+    return (
+    <div className={styles.container}>
+        <form className={styles.form}>
+            <h1 className={styles.title}>Crea tu cuenta</h1>            
+            <div className={styles.inputContainer}>
+                <label htmlFor="nombre">
+                    <span>Nombre de Usuario</span>
+                </label>
+                <input
+                    type="text"
+                    name="nombre"
+                    id="nombre"
+                    placeholder="Jane Doe"
+                />              
+            </div>
+
+            <div className={styles.inputContainer}>
+                <label htmlFor="correo">
+                    <span>Correo Electrónico</span>
+                </label>
+                <input
+                    type="email"
+                    name="correo"
+                    id="correo"
+                    placeholder="ejemplo@gmail.com"
+                />              
+            </div>
+
+            <div className={styles.inputContainer}>
+                <label htmlFor="password">
+                    <span>Contraseña</span>
+                </label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="********"
+                />              
+            </div>
+
+            <button className={styles.submit}
+                type="submit"
+            >
+                Regístrate
+            </button>
+
+            
+            <Link to={loginUrl} className={styles.link}>¿Ya tienes una cuenta? Inicia Sesión aquí</Link>
+            <Link to={homeUrl} className={styles.link}>Regresar a la página principal</Link>
+
+
+        </form>
     </div>
+    );
 }
